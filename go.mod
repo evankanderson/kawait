c1509c19872615cda0bf613fa6c6445c0e4be3b7
Evan Anderson <evan.k.anderson@gmail.com>
2019-06-18 02:21:24 -0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,32 @@
+module github.com/evankanderson/kawait
+
+go 1.12
+
+require (
+	github.com/Azure/go-ansiterm v0.0.0-20170929234023-d6e3b3328b78 // indirect
+	github.com/MakeNowJust/heredoc v0.0.0-20171113091838-e9091a26100e // indirect
+	github.com/Sirupsen/logrus v0.0.0-00010101000000-000000000000 // indirect
+	github.com/docker/docker v1.13.1 // indirect
+	github.com/evanphx/json-patch v4.2.0+incompatible // indirect
+	github.com/exponent-io/jsonpath v0.0.0-20151013193312-d6023ce2651d // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-openapi/spec v0.19.0 // indirect
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
+	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79 // indirect
+	github.com/imdario/mergo v0.3.7 // indirect
+	github.com/knative/pkg v0.0.0-20190531181132-17ec1ed50dad
+	github.com/mattbaird/jsonpatch v0.0.0-20171005235357-81af80346b1a // indirect
+	github.com/mitchellh/go-wordwrap v1.0.0 // indirect
+	github.com/spf13/cobra v0.0.4
+	golang.org/x/net v0.0.0-20190522155817-f3200d17e092 // indirect
+	golang.org/x/oauth2 v0.0.0-20190523182746-aaccbc9213b0 // indirect
+	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
+	k8s.io/apimachinery v0.0.0-20190612125636-6a5db36e93ad
+	k8s.io/cli-runtime v0.0.0-20190612131021-ced92c4c4749 // indirect
+	k8s.io/client-go v0.0.0-20190612125919-5c45477a8ae7
+	k8s.io/klog v0.3.2 // indirect
+	k8s.io/kubernetes v1.14.3
+	k8s.io/utils v0.0.0-20190529001817-6999998975a7 // indirect
+)
+
+replace github.com/Sirupsen/logrus => github.com/sirupsen/logrus v1.4.2
---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/knative/pkg/apis/duck"
+	"github.com/knative/pkg/apis/duck/v1alpha1"
+	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/tools/clientcmd"
+//	"k8s.io/kubernetes/pkg/kubectl/cmd/util"
+)
+
+var cmd = &cobra.Command{
+	Use:   "kawait",
+	Short: "Waits for Kubernetes resources to become ready",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Awaiting %q\n", args)
+
+		loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+		configOverrides := &clientcmd.ConfigOverrides{}
+		kubeConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, configOverrides)
+		config, err := kubeConfig.ClientConfig()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		dynamicClient, err := dynamic.NewForConfig(config)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+//		finder := util.NewCRDFinder(util.CRDFromDynamic(dynamicClient))
+
+		tif := &duck.TypedInformerFactory{
+			Client: dynamicClient,
+			Type:   &v1alpha1.KResource{},
+		}
+		fmt.Printf("Got tif: %v\n", tif)
+
+		for _, item := range args {
+			gvr, name := ParseGVRAndName(item)
+			/*
+				gkName, name := parts[0], parts[1]
+				gk := schema.ParseGroupKind(gkName)
+
+				if ok, err := finder.HasCRD(gk); ok != true || err != nil {
+					fmt.Printf("Unable to find an object of type %q (%v)\n", gkName, gk)
+					continue
+				}
+				// todo gk.WithVersion using version from CRD
+			*/
+			fmt.Printf("Looking for %q, a %v", name, gvr)
+			_, lister, err := tif.Get(gvr)
+			untyped, err := lister.Get(name)
+			if err != nil {
+				fmt.Printf("Failed to fetch %q (%q): %v\n", item, gvr, err)
+				continue
+			}
+			obj := untyped.(*v1alpha1.KResource)
+
+			fmt.Printf("Got it! %v", obj.Status.Conditions)
+		}
+	},
+}
+
+func main() {
+	if err := cmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func ParseGVRAndName(s string) (schema.GroupVersionResource, string) {
+	parts := strings.Split(s, ":")
+	gvrName, name := parts[0], parts[1]
+	parts = strings.Split(gvrName, "/")
+	grName, version := parts[0], parts[1]
+	gr := schema.ParseGroupResource(grName)
+	return gr.WithVersion(version), name
+}
